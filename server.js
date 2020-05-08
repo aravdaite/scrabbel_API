840a596ffb353c10177dbc06edee09d79e22064a
aravdaite <aravdaite@gmail.com>
2020-05-08 21:50:33 +0200

    --put domain to config

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.append('Access-Control-Allow-Origin', 'DOMAIN');
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     res.header("Access-Control-Allow-Credentials", "true");
